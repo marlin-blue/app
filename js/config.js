e27e3dd5398a12ac28148ef4a5d52bc7d5759a16
Jet <jethrohengz@gmail.com>
2023-06-30 10:37:16 +0700

    2t

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_UPL9jE13L', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2hbphak1a3unsdp1nmtq1h3css', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-2' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_AeyT94Cmn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7eks1l5qjm141t3ljghkgfkav6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://guz6cy8mo0.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
